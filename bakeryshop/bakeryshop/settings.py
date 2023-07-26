056a9f0699b73f7bed6dafb7841092135cd2b4bd
Amartyanov1974 <randomquake@yandex.ru>
2023-07-26 14:14:09 +0400

    Update settings.py

--- bakeryshop/bakeryshop/settings.py
+++ bakeryshop/bakeryshop/settings.py
@@ -13,6 +13,10 @@ import os
 from pathlib import Path
 from environs import Env
 
+
+env = Env()
+env.read_env()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
