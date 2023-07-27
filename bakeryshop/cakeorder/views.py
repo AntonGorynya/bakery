e9b03442486103eaec53f0eafcc7b53bacbe9f63
Anton <antongorynya1@gmail.com>
2023-07-27 20:41:20 +0300

    Передал параметры в JS

--- bakeryshop/cakeorder/views.py
+++ bakeryshop/cakeorder/views.py
@@ -3,12 +3,27 @@ from .models import Levels_number, Form, Topping, Berries, Decor
 
 
 def index(request):
+    levels = Levels_number.objects.all()
+    forms = Form.objects.all()
+    toppings = Topping.objects.all()
+    berries = Berries.objects.all()
+    decors = Decor.objects.all()
+
     context = {
-        'levels': [level.quantity for level in Levels_number.objects.all()],
-        'forms': [form for form in Form.objects.all()],
-        'toppings': [topping for topping in Topping.objects.all()],
-        'berries': [berry for berry in Berries.objects.all()],
-        'decors': [decor for decor in Decor.objects.all()],
+        'data': {
+            'levels': [level.quantity for level in levels],
+            'forms': [{'id': form.id, 'name': form.name} for form in forms],
+            'toppings': [{'id': topping.id, 'name': topping.name} for topping in toppings],
+            'berries': [{'id': berry.id, 'name': berry.name} for berry in berries],
+            'decors': [{'id': decor.id, 'name': decor.name} for decor in decors],
+        },
+        'costs': {
+            'levels': [0] + [level.price for level in levels],
+            'forms': [0] + [form.price for form in forms],
+            'toppings': [0] + [topping.price for topping in toppings],
+            'berries': [0] + [berry.price for berry in berries],
+            'decors': [0] + [decor.price for decor in decors],
+        }
     }
     template = loader.get_template('index.html')
     return HttpResponse(template.render(context))
