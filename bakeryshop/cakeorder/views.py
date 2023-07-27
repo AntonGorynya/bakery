dcf22d8973d4fb93df8ca042c0dbbde5594ed3f5
Anton <antongorynya1@gmail.com>
2023-07-27 15:09:31 +0300

    параметризировал шаблон

--- bakeryshop/cakeorder/views.py
+++ bakeryshop/cakeorder/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render, loader, HttpResponse
+from .models import Levels_number, Form, Topping, Berries, Decor
 
 
 def index(request):
-    context = {}
+    context = {
+        'levels': [level.quantity for level in Levels_number.objects.all()],
+        'forms': [form for form in Form.objects.all()],
+        'toppings': [topping for topping in Topping.objects.all()],
+        'berries': [berry for berry in Berries.objects.all()],
+        'decors': [decor for decor in Decor.objects.all()],
+    }
     template = loader.get_template('index.html')
     return HttpResponse(template.render(context))
 
