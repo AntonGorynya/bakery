e0ba749de0c882f3baf5f8a24443c677b58937de
Anton <antongorynya1@gmail.com>
2023-07-31 19:50:31 +0300

    Revert "Artem"

--- bakeryshop/cakeorder/management/commands/create_bitlink.py
+++ bakeryshop/cakeorder/management/commands/create_bitlink.py
@@ -32,5 +32,7 @@ class Command(BaseCommand):
                 response.raise_for_status()
                 bitlink = response.json()["link"]
                 Advertisement.objects.update_or_create(link=link, bitlink=bitlink)
+                print(link)
+                print(bitlink)
         except HTTPError as error:
             logging.exception(error)
